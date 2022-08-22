35b97778462cbc6b377bda48af473b8511347410
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-08-22 17:18:56 +0100

    changed text of nav bar otions, added padding to options

--- Components/NavBar/NavBar.js
+++ Components/NavBar/NavBar.js
@@ -4,24 +4,27 @@ import css from "./NavBar.module.scss";
 function NavBar() {
   return (
     <div className={css.navBar}>
-      <Link href={"#"}>
-        <h4>Home</h4>
-      </Link>
-      <Link href={"#"}>
-        <h4>Services</h4>
-      </Link>
-      <Link href={"#"}>
-        <h4>About Us</h4>
-      </Link>
-      <Link href={"#"}>
-        <h4>Price List</h4>
-      </Link>
-      <Link href={"#"}>
-        <h4>Gallery</h4>
-      </Link>
-      <Link href={"#"}>
-        <h4>Contact Us</h4>
-      </Link>
+      <div className={css.logo}>Logo Pic</div>
+      <div className={css.navOptions}>
+        <Link href={"#"}>
+          <h4>Home</h4>
+        </Link>
+        <Link href={"#"}>
+          <h4>Services</h4>
+        </Link>
+        <Link href={"#"}>
+          <h4>About Us</h4>
+        </Link>
+        <Link href={"#"}>
+          <h4>Price List</h4>
+        </Link>
+        <Link href={"#"}>
+          <h4>Gallery</h4>
+        </Link>
+        <Link href={"#"}>
+          <h4>Contact Us</h4>
+        </Link>
+      </div>
     </div>
   );
 }
