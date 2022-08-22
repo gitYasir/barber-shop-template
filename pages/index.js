e413c60a12291013238eb5a90b1a38f7d9db9782
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-08-22 17:09:11 +0100

    started work on the navBar

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import NavBar from "../Components/NavBar/NavBar";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Barber Shop Name</title>
         <meta name="description" content="Best Barbers around" />
         <link rel="icon" href="/icons/barber-pole-svgrepo-com.svg" />
       </Head>
-    </div>
+      <NavBar />
+    </>
   );
 }
