a7e05e7e1b5b86cf34edfdc0647098c9c43fe17b
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-08-24 10:35:57 +0100

    created homepage component and styled it

---
+++ Components/HomePage/HomePage.js
@@ -0,0 +1,7 @@
+import css from "./HomePage.module.scss";
+
+function HomePage() {
+  return <div className={css.mainContainer}>Home</div>;
+}
+
+export default HomePage;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import HomePage from "../Components/HomePage/HomePage";
 import NavBar from "../Components/NavBar/NavBar";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
         <link rel="icon" href="/icons/barber-pole-svgrepo-com.svg" />
       </Head>
       <NavBar />
+      <HomePage />
     </>
   );
 }
