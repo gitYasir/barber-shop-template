1c0d4df57fe2e0aadfd0d1ea45a027b3fdfcf2dd
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-08-29 17:17:59 +0100

    background imageg sorted

--- Components/HomePage/HomePage.js
+++ Components/HomePage/HomePage.js
@@ -1,8 +1,18 @@
 import css from "./HomePage.module.scss";
+import Image from "next/image";
 
 function HomePage() {
   return (
     <div className={css.mainContainer}>
+      {/* <div style={{ display: "none" }}> */}
+      <Image
+        src={"/pics/agustin-fernandez-Znyjl9pbaUs-unsplash.jpg"}
+        alt={""}
+        layout="fill"
+        priority
+        objectFit="cover"
+      />
+      {/* </div> */}
       <div className={css.subContainer}>
         <a href="tel:11111">
           <button>Call</button>
