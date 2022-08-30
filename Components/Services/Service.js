1f1fe0831cde007efb4cb1eb2095cc0cd753f94e
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-08-30 14:47:41 +0100

    created

--- Components/Services/Service.js
+++ Components/Services/Service.js
@@ -1,7 +1,18 @@
-import React from "react";
+import css from "./Services.module.scss";
+import Image from "next/image";
 
-function Service() {
-  return <div>Service</div>;
+function Service({ pic, alt, info, price }) {
+  return (
+    <div className={css.container}>
+      <div className={css.pic}>
+        <Image src={pic} alt={alt} height={100} width={100} />
+      </div>
+      <div className={css.info}>
+        <p>{info}</p>
+        <h3>{price}</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Service;
--- Components/Services/Services.js
+++ Components/Services/Services.js
@@ -1,7 +1,23 @@
-import React from "react";
+import data from "./data";
+import Service from "./Service";
+import css from "./Services.module.scss";
 
 function Services() {
-  return <div>Services</div>;
+  return (
+    <div className={css.container}>
+      {data.map((item, i) => {
+        return (
+          <Service
+            pic={item.pic}
+            alt={item.alt}
+            info={item.info}
+            price={item.price}
+            key={i}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default Services;
