2f1e05593eb93a0aca9661f8cdee3266dc214bed
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-09-02 21:54:48 +0100

    working on gallery

--- Components/Gallery/Gallery.js
+++ Components/Gallery/Gallery.js
@@ -1,11 +1,23 @@
 import pics from "./galleryData";
 import Image from "next/image";
+import css from "./Gallery.module.scss";
 
 function Gallery() {
   return (
-    <div>
-      {pics.map((pic) => {
-        return <Image src={pic} alt={pic} height={200} width={200} key={pic} />;
+    <div className={css.galleryContainer}>
+      {pics.map((pic, i) => {
+        return (
+          <div key={pic} className={css.picArea}>
+            <Image
+              src={pic}
+              alt={pic}
+              height={200}
+              width={200}
+              className={css.picture}
+            />
+            <p>{i + 1}</p>
+          </div>
+        );
       })}
     </div>
   );
