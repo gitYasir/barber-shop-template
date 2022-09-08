32cd058e6d040b61c0cddab98a7b7ece6017c865
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-09-08 15:40:19 +0100

    made changes to data for services page

--- Components/Services/data.js
+++ Components/Services/data.js
@@ -1,30 +1,48 @@
 const data = [
   {
-    pic: "/pics/agustin-fernandez-Znyjl9pbaUs-unsplash.jpg",
+    pic: "/gallery/Black-Men-Haircuts-Long-Top-Short.jpg",
     alt: "test",
     info: `From a full head shave to a beard trim, a hair cut, clipper cut, or entire shaving, our barbers have you well taken care of. An attractive range of styles and designs are offered at our Shop, including kids hair cut. Whether the style you desire is yours or the expert's, you are bound to leave in picture-perfect condition.`,
     price: "£14.99",
   },
   {
-    pic: "/pics/agustin-fernandez-Znyjl9pbaUs-unsplash.jpg",
+    pic: "/gallery/Black-Men-Haircuts-Low-Fade-With-Twists.jpg",
     alt: "test",
     info: `From a full head shave to a beard trim, a hair cut, clipper cut, or entire shaving, our barbers have you well taken care of. An attractive range of styles and designs are offered at our Shop, including kids hair cut. Whether the style you desire is yours or the expert's, you are bound to leave in picture-perfect condition.`,
     price: "£14.99",
   },
   {
-    pic: "/pics/agustin-fernandez-Znyjl9pbaUs-unsplash.jpg",
+    pic: "/gallery/michael-demoya-Q82AM6BWBPM-unsplash.jpg",
     alt: "test",
     info: `From a full head shave to a beard trim, a hair cut, clipper cut, or entire shaving, our barbers have you well taken care of. An attractive range of styles and designs are offered at our Shop, including kids hair cut. Whether the style you desire is yours or the expert's, you are bound to leave in picture-perfect condition.`,
     price: "£14.99",
   },
   {
-    pic: "/pics/agustin-fernandez-Znyjl9pbaUs-unsplash.jpg",
+    pic: "/gallery/pexels-rodnae-productions-7697394.jpg",
     alt: "test",
     info: `From a full head shave to a beard trim, a hair cut, clipper cut, or entire shaving, our barbers have you well taken care of. An attractive range of styles and designs are offered at our Shop, including kids hair cut. Whether the style you desire is yours or the expert's, you are bound to leave in picture-perfect condition.`,
     price: "£14.99",
   },
   {
-    pic: "/pics/agustin-fernandez-Znyjl9pbaUs-unsplash.jpg",
+    pic: "/gallery/Black-Men-Haircuts-Long-Top-Short.jpg",
+    alt: "test",
+    info: `From a full head shave to a beard trim, a hair cut, clipper cut, or entire shaving, our barbers have you well taken care of. An attractive range of styles and designs are offered at our Shop, including kids hair cut. Whether the style you desire is yours or the expert's, you are bound to leave in picture-perfect condition.`,
+    price: "£14.99",
+  },
+  {
+    pic: "/gallery/Black-Men-Haircuts-Low-Fade-With-Twists.jpg",
+    alt: "test",
+    info: `From a full head shave to a beard trim, a hair cut, clipper cut, or entire shaving, our barbers have you well taken care of. An attractive range of styles and designs are offered at our Shop, including kids hair cut. Whether the style you desire is yours or the expert's, you are bound to leave in picture-perfect condition.`,
+    price: "£14.99",
+  },
+  {
+    pic: "/gallery/michael-demoya-Q82AM6BWBPM-unsplash.jpg",
+    alt: "test",
+    info: `From a full head shave to a beard trim, a hair cut, clipper cut, or entire shaving, our barbers have you well taken care of. An attractive range of styles and designs are offered at our Shop, including kids hair cut. Whether the style you desire is yours or the expert's, you are bound to leave in picture-perfect condition.`,
+    price: "£14.99",
+  },
+  {
+    pic: "/gallery/pexels-rodnae-productions-7697394.jpg",
     alt: "test",
     info: `From a full head shave to a beard trim, a hair cut, clipper cut, or entire shaving, our barbers have you well taken care of. An attractive range of styles and designs are offered at our Shop, including kids hair cut. Whether the style you desire is yours or the expert's, you are bound to leave in picture-perfect condition.`,
     price: "£14.99",
